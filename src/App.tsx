d191bd01ac05d39d677d8fa45258cea2ea2af06b
Denis Turbin <denisturbin@MacBook-Pro-Denis.local>
2026-03-11 12:14:18 +0400

    fix: replace duration with speed prop

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
 
   const text = params.get('text') ?? 'Hello World'
   const color = params.get('color') ?? '#000000'
-  const duration = Number(params.get('duration') ?? 3)
+  const speed = params.get('speed') != null ? Number(params.get('speed')) : undefined
   const fontUrl = params.get('fontUrl') ?? '/fonts/BrittanySignature.ttf'
 
   return (
     <Penflow
       text={text}
       color={color}
-      duration={duration}
+      speed={speed}
       fontUrl={fontUrl}
     />
   )
