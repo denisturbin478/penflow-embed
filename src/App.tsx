1ddb784a07baeff26ef04e9eee2015b9667a7e41
Denis Turbin <denisturbin@MacBook-Pro-Denis.local>
2026-03-11 12:57:44 +0400

    add size param and fix centering

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ function App() {
   const text = params.get('text') ?? 'Hello World'
   const color = params.get('color') ?? '#000000'
   const bgColor = params.get('bgColor') ?? 'transparent'
+  const size = params.get('size') ?? '120'
   const speed = params.get('speed') != null ? Number(params.get('speed')) : undefined
   const fontUrl = params.get('fontUrl') ?? '/fonts/BrittanySignature.ttf'
 
   return (
-    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '40px 20px 80px 20px', backgroundColor: bgColor }}>
+    <div style={{ width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '40px 20px 80px 20px', backgroundColor: bgColor }}>
       <Penflow
         text={text}
         color={color}
+        size={Number(size)}
         speed={speed}
         fontUrl={fontUrl}
       />
