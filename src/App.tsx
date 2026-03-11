99190dc75a8177a585a4622053bbd1490a43b80a
Denis Turbin <denisturbin@MacBook-Pro-Denis.local>
2026-03-11 12:54:45 +0400

    center text and add bgColor param

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ function App() {
 
   const text = params.get('text') ?? 'Hello World'
   const color = params.get('color') ?? '#000000'
+  const bgColor = params.get('bgColor') ?? 'transparent'
   const speed = params.get('speed') != null ? Number(params.get('speed')) : undefined
   const fontUrl = params.get('fontUrl') ?? '/fonts/BrittanySignature.ttf'
 
   return (
-    <div style={{ alignItems: 'center', justifyContent: 'center', padding: '40px 20px 80px 20px' }}>
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '40px 20px 80px 20px', backgroundColor: bgColor }}>
       <Penflow
         text={text}
         color={color}
